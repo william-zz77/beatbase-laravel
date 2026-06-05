a1b22deee192f93dfce92637ffb535f5cec7f4ea
Rehan <24082010040@student.upnjatim.ac.id>
2026-06-05 23:28:45 +0700

    migrasi user dan reservasi

--- database/migrations/2026_06_05_150201_create_reservasi_table.php
+++ database/migrations/2026_06_05_150201_create_reservasi_table.php
@@ -10,12 +10,18 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('reservasi', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('reservasi', function (Blueprint $table) {
+        $table->id('id_reservasi');
+        $table->foreignId('id_user')->constrained('users', 'id')->onDelete('cascade');
+        $table->foreignId('id_studio')->constrained('studio', 'id_studio')->onDelete('cascade');
+        $table->date('tanggal');
+        $table->time('jam_mulai');
+        $table->time('jam_selesai');
+        $table->enum('status', ['pending', 'confirmed', 'cancelled'])->default('pending');
+        $table->timestamp('created_at')->useCurrent();
+    });
+}
 
     /**
      * Reverse the migrations.
