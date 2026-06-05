aa5a0772a4fc28fb757319c72f2f587e9483f8b5
William Hia <24082010043@student.upnjatim.ac.id>
2026-06-06 00:18:15 +0700

    menambahkan model reservasi dan studio

--- app/Models/Reservasi.php
+++ app/Models/Reservasi.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reservasi extends Model
 {
-    //
+    protected $table = 'reservasi';
+    protected $primaryKey = 'id_reservasi';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_user',
+        'id_studio',
+        'tanggal',
+        'jam_mulai',
+        'jam_selesai',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user', 'id');
+    }
+
+    public function studio()
+    {
+        return $this->belongsTo(Studio::class, 'id_studio', 'id_studio');
+    }
 }
--- app/Models/Studio.php
+++ app/Models/Studio.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Studio extends Model
 {
-    //
+    protected $table = 'studio';
+    protected $primaryKey = 'id_studio';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nama_studio',
+        'harga_per_jam',
+    ];
+
+    public function reservasi()
+    {
+        return $this->hasMany(Reservasi::class, 'id_studio', 'id_studio');
+    }
 }
