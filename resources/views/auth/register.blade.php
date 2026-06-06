123a2df3cc42b771426404c7d57fb2d43802136b
Rehan <24082010040@student.upnjatim.ac.id>
2026-06-06 19:50:23 +0700

    menambahkan role pada register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -33,6 +33,14 @@
                     required>
             </div>
             <div class="mb-6">
+                <label class="block text-gray-700 mb-1">Daftar sebagai</label>
+                <select name="role"
+                    class="w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400">
+                    <option value="customer" {{ old('role') == 'customer' ? 'selected' : '' }}>Customer</option>
+                    <option value="owner" {{ old('role') == 'owner' ? 'selected' : '' }}>Owner</option>
+                </select>
+            </div>
+            <div class="mb-6">
                 <label class="block text-gray-700 mb-1">Konfirmasi Password</label>
                 <input type="password" name="password_confirmation"
                     class="w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
