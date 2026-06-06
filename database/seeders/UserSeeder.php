2cd13009b70746b473451693eeebfad53c8f330c
William Hia <24082010043@student.upnjatim.ac.id>
2026-06-06 18:26:49 +0700

    menambahkan isi pada file user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,33 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        User::create([
+            'nama'     => 'Administrator',
+            'email'    => 'admin@beatbase.com',
+            'password' => Hash::make('password'),
+            'role'     => 'admin',
+        ]);
+
+        User::create([
+            'nama'     => 'Owner Studio',
+            'email'    => 'owner@beatbase.com',
+            'password' => Hash::make('password'),
+            'role'     => 'owner',
+        ]);
+
+        User::create([
+            'nama'     => 'Customer Test',
+            'email'    => 'customer@beatbase.com',
+            'password' => Hash::make('password'),
+            'role'     => 'customer',
+        ]);
     }
 }
