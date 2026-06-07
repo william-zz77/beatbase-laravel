c4be4a91a88fa892538a30fbabb38b0f02fabf85
William Hia <24082010043@student.upnjatim.ac.id>
2026-06-07 16:23:29 +0700

    memperbaiki route

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // Admin routes
 Route::middleware(['auth', 'role:admin', 'session.timeout'])->prefix('admin')->name('admin.')->group(function () {
